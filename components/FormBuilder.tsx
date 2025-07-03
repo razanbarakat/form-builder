b310e193c885bdab0f1b63a0d657809fcaf307ce
razanbarakat <razan.alfeelat@gmail.com>
2025-07-04 00:02:49 +0300

    add paper svg

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -7,7 +7,7 @@ import PublishFormBtn from "./PublishFormBtn";
 export default function FormBuilder({ form }: { form: Form }) {
   return (
     <main className="flex flex-col w-full">
-      <div className="flex justify-between border-b-2 p-4 gap-3 items-center">
+      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
         <h2 className="truncate  font-medium">
           <span className="text-muted-foreground mr-2">Form:</span>
           {form.name}
@@ -21,6 +21,10 @@ export default function FormBuilder({ form }: { form: Form }) {
             </>
           )}
         </div>
+      </nav> 
+      <div className="flex w-full flex-grow items-center justify-center relative overflow-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+      <Designer/>
+      
       </div>
     </main>
   );
